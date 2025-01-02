bef8d2b92fa43f020ed186190bc2401b4fe0fa13
Jarosław Dutka <jaroslawjdutka@gmail.com>
2025-01-02 15:50:21 +0100

    WIP

--- FiatChampAddon/FiatClient/Infrastructure/PollyRequestHandler.cs
+++ FiatChampAddon/FiatClient/Infrastructure/PollyRequestHandler.cs
@@ -21,16 +21,19 @@ public class PollyRequestHandler : DelegatingHandler
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) => 
         await _policy.ExecuteAsync((_, ct) => base.SendAsync(request, ct), new Context { { "RequestUrl", request.RequestUri?.ToString() } }, cancellationToken);
 
-    private void OnRetry(DelegateResult<HttpResponseMessage> delegateResult, TimeSpan retryAfter, int retryCount, Context context)
+    private async Task OnRetry(DelegateResult<HttpResponseMessage> delegateResult, TimeSpan retryAfter, int retryCount, Context context)
     {
         var requestUrl = context["RequestUrl"];
         var exception = delegateResult.Exception as HttpRequestException;
         var reason = delegateResult.Result?.StatusCode.ToString() ??
                      exception?.StatusCode?.ToString() ??
                      exception?.Message;
+
+        var payload = await (delegateResult.Result?.Content.ReadAsStringAsync() ?? Task.FromResult(""));
+
         if (_logger.IsEnabled(LogLevel.Debug))
-            _logger.LogDebug(exception, "Error connecting to {url}. Reason: {reason}. Retrying in {retry}", requestUrl, reason, retryAfter);
+            _logger.LogDebug(exception, "Error connecting to {url}. Reason: {reason}. Retrying in {retry}. Payload: {payload}", requestUrl, reason, retryAfter, payload);
         else
-            _logger.LogWarning("Error connecting to {url}. Reason: {reason}. Retrying in {retry}", requestUrl, reason, retryAfter);
+            _logger.LogWarning("Error connecting to {url}. Reason: {reason}. Retrying in {retry}. Payload: {payload}", requestUrl, reason, retryAfter, payload);
     }
 }
